e3375d86e88df49f1099bfe0794f9c594a9f8077
Yuliia <yteren@softserveinc.com>
2023-08-31 17:31:51 +0200

    commit files

--- src/main/java/com/automationteststore/pages/ShoppingCartPage.java
+++ src/main/java/com/automationteststore/pages/ShoppingCartPage.java
@@ -27,4 +27,5 @@ public class ShoppingCartPage {
         WebElement quantityInput = driver.findElement(By.xpath(quantity));
         return Integer.parseInt(quantityInput.getAttribute("value"));
     }
+
 }
