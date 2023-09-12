f1ff092e3017698fb0cc9912b368f42c13d8cc75
Yuliia <yteren@softserveinc.com>
2023-09-12 10:21:45 +0200

    valid login tests

--- src/test/java/com/automationstore/login_tests/LoginTests.java
+++ src/test/java/com/automationstore/login_tests/LoginTests.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 
 
 public class LoginTests {
---
+++ src/test/java/com/automationstore/training_tests/FirstScriptTest.java
@@ -0,0 +1,2 @@
+package com.automationstore.training_tests;public class FirstScriptTest {
+}
